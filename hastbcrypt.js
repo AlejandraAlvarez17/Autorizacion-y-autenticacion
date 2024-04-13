09c15ea2fabc7732f0cdbbb1c532a91b846ebcd4
AlejandraAlvarez17 <116087716+AlejandraAlvarez17@users.noreply.github.com>
2024-04-13 23:17:45 +0200

    Add files via upload

---
+++ hastbcrypt.js
@@ -0,0 +1,25 @@
+
+
+//1) Instalamos: npm install bcrypt
+//2) Importamos el módulo. 
+
+import bcrypt from "bcrypt";
+
+//Vamos a crear dos funciones: 
+
+//a) createHash: aplicar el hash al password. 
+//b) isValidPassword: comparar el password proporcionado por la base de datos. 
+
+const createHash = password => bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+//hashSync: toma el password que le pasamos y aplica el proceso de hasheo a partir de un salt. 
+
+//Un "salt" es un string random que hacer que el proceso se realice de forma impredecible. 
+
+//Este proceso es irreversible!! nada que hacer
+
+const isValidPassword = (password, user) => bcrypt.compareSync(password, user.password);
+
+//Compara los password, retorna true o falsete segun corresponda. 
+
+export {createHash, isValidPassword};
